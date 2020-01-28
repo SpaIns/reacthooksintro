b4c8f1575b56ea7188d12b64e3d34a0d7bbde54d
Steffan <sachtmann121@gmail.com>
2020-01-28 00:09:24 -0800

    Updated useState usage to be correct for ingredient form

--- components/Ingredients/IngredientForm.js
+++ components/Ingredients/IngredientForm.js
@@ -8,6 +8,7 @@ const IngredientForm = React.memo(props => {
   // Returns an array with exactly 2 element.
   // 1) Current state snapshot for this re-render
   // 2) Function that allows update of current state
+  //    Updated state by replacing entire state. Not just a portion of state
   const inputState = useState({title: '', amount: ''})
 
   const submitHandler = event => {
@@ -15,6 +16,8 @@ const IngredientForm = React.memo(props => {
     // ...
   };
 
+  // Note we need to do onChange using prev state, since using cur state
+  //opens us up to errors re: react not updating state instantly/inorder
   return (
     <section className="ingredient-form">
       <Card>
@@ -22,12 +25,26 @@ const IngredientForm = React.memo(props => {
           <div className="form-control">
             <label htmlFor="title">Name</label>
             <input type="text" id="title" value={inputState[0].title}
-            onChange={event => inputState[1]({title: event.target.value})}/>
+            onChange={event => {
+              // Since event isn't always updating the way that might
+              // be intuitive, we need to store it seperately
+              // to prevent errors where the inner function
+              // doesn't have access to the current event
+              const newTitle = event.target.value
+              inputState[1]((prevInputState) => 
+              ({title: newTitle, amount: prevInputState.amount})
+              )
+            }
+            }/>
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
             <input type="number" id="amount" value={inputState[0].amount}
-            onChange={event => inputState[1]({amount: event.target.value})}/>
+            onChange={event => {
+              const newAmount = event.target.value
+              inputState[1]((prevInputState) => 
+              ({title: prevInputState.title, amount: newAmount}))
+              }}/>
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
