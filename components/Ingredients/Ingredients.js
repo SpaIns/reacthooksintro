133178f8793aecd1dbe406925b12a79f224fe815
Steffan <sachtmann121@gmail.com>
2020-01-28 23:22:00 -0800

    Added a post request to send our ingredients to firebase
    Adds request to gather ings from database using useEffect hook

--- components/Ingredients/Ingredients.js
+++ components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 import IngredientForm from './IngredientForm';
 import Search from './Search';
@@ -7,10 +7,57 @@ import IngredientList from './IngredientList'
 // could also be written const ing = () => 
 function Ingredients() {
   const [ings, setIngs] = useState([])
+  const firebase_url = process.env.REACT_APP_FIREBASE_HOOKS
+  const fetchIngsUrl = firebase_url + 'ingredients.json'
+
+  // Used to manage side effects - such as HTTP requests
+  // Triggered after every render cycle, for every render cycle.
+  // Works like componentDidUpdate
+  useEffect( () =>
+    fetch(fetchIngsUrl).then(response => 
+      response.json().then(responseData => {
+        const loadedIngs = []
+        for (const key in responseData) {
+          loadedIngs.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount
+          })
+        }
+        setIngs(loadedIngs)
+      })
+    ).catch(err => console.log(err)),
+    // Second arg is array of dependencies.
+    // If left empty, acts as componentDidMount;
+    //  runs only once after first render
+    [fetchIngsUrl]
+  )
+
+  // Can use useEffect/useState as much as you want.
+  useEffect(() => {
+    console.log('RENDERING', ings)
+  }, [ings])
+  // ^ means only re-runs when ings changes
+
+  // If you fetch as you render, the state updates. Then re-renders. Then you fetch
+  // End up in infinite loop, thus use useEffect
 
   const addIngHandler = ingredient => {
-    setIngs(prevIngs => [...prevIngs, 
-      {id: Math.random().toString(), ...ingredient}])
+    // Fetch by default sends GET request; firebase requires POST
+    fetch(firebase_url + 'ingredients.json', {
+      method: 'POST',
+      body: JSON.stringify(ingredient),
+      headers: {'Content-Type': 'application/json'},
+    }).then(response => {
+      // We want the json version of the response; another promise returned
+      return response.json().then(responseData => {
+        // Response data will contain ID firebase generates
+        setIngs(prevIngs => [...prevIngs, 
+          {id: responseData.name, ...ingredient}])
+      })
+    }).catch(err => {
+      console.log(err)
+    })
   }
 
   // Removes ingredient from list on click. Use ID as thing
